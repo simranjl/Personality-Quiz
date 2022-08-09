b323caabb978fb4b1a6fb454e3872143f8bd1123
simranjl <90574188+simranjl@users.noreply.github.com>
2022-08-09 01:26:19 -0400

    Add files via upload

---
+++ Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Simran Jaikumar Lekhwani on 5/11/19.
+//  Copyright © 2019 Simran Jaikumar Lekhwani. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Personality Quiz/Personality Quiz/ResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsViewController.swift
+//  Personality Quiz
+//
+//  Created by Simran Jaikumar Lekhwani on 5/14/19.
+//  Copyright © 2019 Simran Jaikumar Lekhwani. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
